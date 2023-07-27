312ec77d60a6f57af0101425f7f60c1ec8c46306
Lolmoag <Pc130@DESKTOP-0U1I54Q>
2023-07-27 12:19:08 +0200

    le rajout du polonium ingot/dust

--- src/main/java/net/mcreator/uraniumcraft/init/UraniumcraftModItems.java
+++ src/main/java/net/mcreator/uraniumcraft/init/UraniumcraftModItems.java
@@ -12,10 +12,12 @@ import net.minecraftforge.common.ForgeSpawnEggItem;
 import net.minecraft.world.item.Item;
 
 import net.mcreator.uraniumcraft.item.PoloniumingotItem;
+import net.mcreator.uraniumcraft.item.PoloniumdustItem;
 import net.mcreator.uraniumcraft.UraniumcraftMod;
 
 public class UraniumcraftModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, UraniumcraftMod.MODID);
 	public static final RegistryObject<Item> ELEMENTALGOLEM_SPAWN_EGG = REGISTRY.register("elementalgolem_spawn_egg", () -> new ForgeSpawnEggItem(UraniumcraftModEntities.ELEMENTALGOLEM, -10079488, -16751104, new Item.Properties()));
 	public static final RegistryObject<Item> POLONIUMINGOT = REGISTRY.register("poloniumingot", () -> new PoloniumingotItem());
+	public static final RegistryObject<Item> POLONIUMDUST = REGISTRY.register("poloniumdust", () -> new PoloniumdustItem());
 }
